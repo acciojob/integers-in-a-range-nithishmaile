16fb2548c600d4e755adcb8af6cb297ac1fbdfb0
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2026-01-05 18:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // write your code here
+function range(start,end){
+	let newArray=[]
+	for(let i=start;i<=end;i++){
+		newArray.push(i)
+	}
+	return newArray
+}
 
-// do not change the code below.
+
+// do not change the code below
 let start = parseInt(prompt("Enter Start of the Range."));
 let end = parseInt(prompt("Enter End Of the Range."));
 alert(range(start, end));
